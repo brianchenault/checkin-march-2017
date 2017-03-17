2b019ed0e77d326d8cb66fe71678b4d361328377
Brian Chenault <brian1@Brians-MacBook-Pro.local>
2017-03-17 13:08:53 -0600

    more slides and images

--- presentation/index.js
+++ presentation/index.js
@@ -25,43 +25,7 @@ require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
 
-const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png"),
-  jackfruitbbq: require('../assets/pics/jackfruitbbq.jpg'),
-  baconish: require('../assets/pics/baconish.jpg'),
-  cauliflower: require('../assets/pics/cauliflower.jpg'),
-  kalebfast: require('../assets/pics/kalebfast.jpg'),
-  asian: require('../assets/pics/asian.jpg'),
-  ip: require('../assets/pics/ip.jpg'),
-  pantry: require('../assets/pics/pantry.jpg'),
-  risotto: require('../assets/pics/risotto.jpg'),
-  sting1: require('../assets/pics/sting1.jpg'),
-  sting2: require('../assets/pics/sting2.jpg'),
-  sting3: require('../assets/pics/sting3.jpg'),
-  sting4: require('../assets/pics/sting4.jpg'),
-  taos1: require('../assets/pics/taos.jpg'),
-  taos2: require('../assets/pics/taos2.jpg'),
-  taos3: require('../assets/pics/taos3.jpg'),
-  taos4: require('../assets/pics/taos4.jpg'),
-  taos5: require('../assets/pics/taos5.jpg'),
-  taos6: require('../assets/pics/taos6.jpg'),
-  taos7: require('../assets/pics/taos7.jpg'),
-  taos8: require('../assets/pics/taos8.jpg'),
-  sand1: require('../assets/pics/sand1.jpg'),
-  sand2: require('../assets/pics/sand2.jpg'),
-  sand3: require('../assets/pics/sand3.jpg'),
-  sand4: require('../assets/pics/sand4.jpg'),
-  mountains1: require('../assets/pics/mountain1.jpg'),
-  mountains2: require('../assets/pics/mountain2.jpg'),
-  mountains3: require('../assets/pics/mountain3.jpg'),
-  mountains4: require('../assets/pics/mountains4.jpg'),
-  mountains5: require('../assets/pics/mountains5.jpg'),
-  mountains6: require('../assets/pics/mountains6.jpg'),
-  mountains7: require('../assets/pics/mountains7.jpg'),
-};
+import images from './images'
 
 preloader(images);
 
@@ -259,6 +223,80 @@ export default class Presentation extends React.Component {
               <div style={imageContainerStyle3}>
                 <img src={images.sting4} style={imageStyle} />
               </div>
+            </div>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <Heading size={5} textColor="primary" caps>Mountains</Heading>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <BlockQuote>
+              <Quote style={{ fontSize: '28px'}}>"Keep close to Nature’s heart… and break clear away, once in a while, and climb a mountain or spend a week in the woods. Wash your spirit clean."</Quote>
+              <Cite>John Muir</Cite>
+            </BlockQuote>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <BlockQuote>
+              <Quote>"Perhaps the truth depends on a walk around the lake."</Quote>
+              <Cite>Wallace Stevens</Cite>
+            </BlockQuote>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+              <div style={imageContainerStyle3}>
+                <img src={images.mountains1} style={imageStyle} />
+              </div>
+              <div style={imageContainerStyle3}>
+                <img src={images.mountains5} style={imageStyle} />
+              </div>
+            </div>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+              <div style={imageContainerStyle3}>
+                <img src={images.mountains3} style={imageStyle} />
+              </div>
+              <div style={imageContainerStyle3}>
+                <img src={images.mountains2} style={imageStyle} />
+              </div>
+            </div>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+              <div style={imageContainerStyle3}>
+                <img src={images.mountains4} style={imageStyle} />
+              </div>
+              <div style={imageContainerStyle3}>
+                <img src={images.mountains6} style={imageStyle} />
+              </div>
+            </div>
+          </Slide>
+
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap'}}>
+              <div style={imageContainerStyle3}>
+                <img src={images.mountains7} style={imageStyle} />
+              </div>
+              <div style={imageContainerStyle3}>
+                <img src={images.mountains8} style={imageStyle} />
+              </div>
+            </div>
+          </Slide>
+
+
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <Heading size={5} textColor="primary" caps>Dance Lessons</Heading>
+            <div style={{
+                width: '100%',
+                height: 'auto',
+                margin: '40px auto 0 auto'
+              }}>
+              <img src={images.fred} style={imageStyle} />
             </div>
           </Slide>
 
